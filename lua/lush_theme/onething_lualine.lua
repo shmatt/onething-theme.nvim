111beca282525018ec007b1f76ddd709f58db268
Matt Tew <matt.tew@intellis.team>
2024-02-06 21:15:32 +0800

    Added lualine

---
+++ lua/lush_theme/onething_lualine.lua
@@ -0,0 +1,49 @@
+
+local M = {}
+M.theme = function()
+  local onething_colours = require("lush_theme.onething-colours")
+  local colors = {
+    darkgray = onething_colours.white.mix(onething_colours.black, 80).hex,
+    gray = onething_colours.gray.hex,
+    innerbg = nil,
+    outerbg = onething_colours.white.mix(onething_colours.black, 90).hex,
+    normal = onething_colours.gray.hex,
+    insert = onething_colours.white.hex,
+    visual = onething_colours.accent.hex, 
+    replace = onething_colours.error.hex, 
+    command = onething_colours.warning.hex,
+  }
+  return {
+    inactive = {
+      a = { fg = colors.gray, bg = colors.outerbg, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.outerbg },
+      c = { fg = colors.gray, bg = colors.innerbg },
+    },
+    visual = {
+      a = { fg = colors.darkgray, bg = colors.visual, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.outerbg },
+      c = { fg = colors.gray, bg = colors.innerbg },
+    },
+    replace = {
+      a = { fg = colors.darkgray, bg = colors.replace, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.outerbg },
+      c = { fg = colors.gray, bg = colors.innerbg },
+    },
+    normal = {
+      a = { fg = colors.darkgray, bg = colors.normal, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.outerbg },
+      c = { fg = colors.gray, bg = colors.innerbg },
+    },
+    insert = {
+      a = { fg = colors.darkgray, bg = colors.insert, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.outerbg },
+      c = { fg = colors.gray, bg = colors.innerbg },
+    },
+    command = {
+      a = { fg = colors.darkgray, bg = colors.command, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.outerbg },
+      c = { fg = colors.gray, bg = colors.innerbg },
+    },
+  }
+end
+return M
